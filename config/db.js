97c3f18fc6a3a71fdbbb5ac07aed05fdd0063253
SAIKIRANRANGUSAI <saikiranrangu6@gmail.com>
2025-10-24 12:36:17 +0530

    added .env

--- config/db.js
+++ config/db.js
@@ -1,20 +1,35 @@
+// const mysql = require("mysql2/promise");
+// require("dotenv").config();
+
+// const db = mysql.createPool({
+//   // host: process.env.DB_HOST,
+//   // user: process.env.DB_USER,
+//   // password: process.env.DB_PASSWORD,
+//   // database: process.env.DB_NAME,
+//   // port: process.env.DB_PORT || 3306,
+//   host: "46.250.225.169",
+//   user: "demo_colormo_usr",
+//   password: "QRdKdVpp3pnNhXBt",
+//   database: "my_loan_bazar",
+//   port: "3306",
+
+
+// });
+
+
+// module.exports = db;
+
+
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const db = mysql.createPool({
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME,
-  // port: process.env.DB_PORT || 3306,
-  host: "46.250.225.169",
-  user: "demo_colormo_usr",
-  password: "QRdKdVpp3pnNhXBt",
-  database: "my_loan_bazar",
-  port: "3306",
-
-
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
 });
 
-
 module.exports = db;
+
